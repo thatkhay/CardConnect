26ef77fa955ff5494ebd65f5b683814310826c6d
thatkhay <johnkingsleyegeonu@gmail.com>
2023-06-25 21:56:58 +0100

    inputs created

--- cardconnect/src/components/RightBackgroundImg.jsx
+++ cardconnect/src/components/RightBackgroundImg.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Form from '@mui/material/Form';
 
 function RightBackgroundImg() {
   return (
@@ -14,7 +16,17 @@ function RightBackgroundImg() {
         flexDirection: 'column',
       }}
     >
-ibd
+      <div style={{display: 'flex', flexDirection: 'column'}}>
+      <TextField id="outlined-basic" label="CARDHOLDER NAME" variant="outlined" style={{marginBottom: '1rem'}} inputProps={{ inputMode: 'alphapets' }}/>
+<TextField id="outlined-basic" label="CARD NUMBER" variant="outlined" style={{marginBottom: '1rem'}}/>
+      </div>
+
+      <div style={{display: 'flex'}}>
+      <TextField id="outlined-basic" label="MM" variant="outlined" style={{marginBottom: '1rem', width: '4rem', marginRight: '1rem',marginTop: '1rem'}} inputProps={{ inputMode: 'numeric' }} />
+      <TextField id="outlined-basic" label="YY" variant="outlined" style={{marginBottom: '1rem', width: '4rem', marginRight: '1rem',marginTop: '1rem'}} inputProps={{ inputMode: 'numeric' }}/>
+      <TextField id="outlined-basic" label="CVC" variant="outlined" style={{marginBottom: '1rem', width: '7rem',marginTop: '1rem'}} inputProps={{ inputMode: 'numeric' }}/>
+      </div>
+      <Button variant="contained" style={{width: '20rem',marginTop: '1rem', backgroundColor: 'black'}}>confirm</Button>
     </div>
   );
 }
